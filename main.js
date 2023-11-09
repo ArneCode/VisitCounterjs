bc2b10f039fc6e8f316cee22f45175d621a1daec
ArneCode <62389012+ArneCode@users.noreply.github.com>
2023-11-09 20:09:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ let counter = 0
 const server = http.createServer((req, res) => {
     counter = counter + 1
   // Check if the request URL is for the root path
+  const parsedUrl = url.parse(req.url);
+  const query = querystring.parse(parsedUrl.query);
+  console.log(query)
   res.writeHead(500, { 'Content-Type': 'text/plain' });
   res.end(`Counter new: ${counter}`);
 });
